f75cd95e6c7f0f6ebb15ab17c27e4197714432b1
nikeetaa <nikki231.nc@gmail.com>
2020-03-25 19:42:18 +0530

    adding auth status listener

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from './auth.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: "root"
@@ -9,11 +10,16 @@ import { AuthData } from './auth.model';
 export class AuthService {
 
   private token: string;
+  private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient) { }
 
   getToken() {
     return this.token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
   }
 
   createUser(email: string, password: string) {
@@ -35,6 +41,7 @@ export class AuthService {
       .subscribe(response => {
         const token = response.token;
         this.token = token;
-      })
+        this.authStatusListener.next(true);
+    })
   }
 }
